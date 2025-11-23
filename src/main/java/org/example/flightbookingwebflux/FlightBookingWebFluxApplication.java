904fc4402ed49f4f97e680b68a5636c49ea933f0
vi2hnu <vishnuvaradhankr@gmail.com>
2025-11-23 11:27:58 +0530

    initial commit

---
+++ src/main/java/org/example/flightbookingwebflux/FlightBookingWebFluxApplication.java
@@ -0,0 +1,13 @@
+package org.example.flightbookingwebflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlightBookingWebFluxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlightBookingWebFluxApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/flightbookingwebflux/FlightBookingWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.flightbookingwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlightBookingWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
